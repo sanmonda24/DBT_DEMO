07738f63b2c8b31461784a39c9f0ff4da8331372
oracleoracledba@gmail.com <oracleoracledba@gmail.com>
2025-12-04 14:05:30 +0000

    station_diamension commit

---
+++ models/example/station_diamension.sql
@@ -0,0 +1,14 @@
+WITH BIKE as (
+
+select
+START_STATIO_ID as station_id,
+START_STATION_NAME as station_name,
+START_LAT as station_lat,
+START_LNG as station_lng
+from {{ source('snowflake', 'bike') }}
+
+)
+
+select 
+*
+from  BIKE
